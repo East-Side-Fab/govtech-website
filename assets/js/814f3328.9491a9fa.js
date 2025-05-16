84241d64068bc357d18da5f76684d479d8ed1220
huhn511 <huhn511@users.noreply.github.com>
2025-05-16 15:33:02 +0000

    deploy: 2ef9845ac0f05a371ca5651293181ee2711c4ed0

--- assets/js/814f3328.9491a9fa.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgovhack_website=self.webpackChunkgovhack_website||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Willkommen zum GovTech Hackathon!","permalink":"/govtech-website/blog/2025/02/22/willkommen-zum-govtech-hackathon","unlisted":false,"date":"2025-02-22T00:00:00.000Z"}]}')}}]);
